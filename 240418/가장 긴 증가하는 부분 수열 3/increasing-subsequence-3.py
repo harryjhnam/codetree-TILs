022a1ebfdf9755930fcd0740210a053ccb7b2684
JuhyeonNam <harryjhnam@gist.ac.kr>
2024-04-18 08:46:41 +0900

    Title: 가장 긴 증가하는 부분 수열 3, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240418/가장 긴 증가하는 부분 수열 3/increasing-subsequence-3.py
+++ 240418/가장 긴 증가하는 부분 수열 3/increasing-subsequence-3.py
@@ -5,11 +5,6 @@ dp = [0] * (n+1)
 
 for i in range(1, n+1):
     cur, prev = seq[i], seq[i-1]
-    if cur > prev:
-        # increasing
-        dp[i] = dp[i-1] + 1
-    else:
-        # not increasing
-        dp[i] = max([l for l, prev in zip(dp[:i], seq[:i]) if cur > prev]) + 1
+    dp[i] = max([l for l, prv in zip(dp[:i], seq[:i]) if cur > prv]) + 1
 
 print(max(dp))
