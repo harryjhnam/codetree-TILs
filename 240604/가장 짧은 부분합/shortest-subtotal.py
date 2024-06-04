6639a73942a1ba76bec7306d8fe008f2e18bcdf8
JuhyeonNam <harryjhnam@gist.ac.kr>
2024-06-04 21:32:36 +0900

    Title: 가장 짧은 부분합, Time: 5087ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 240604/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,15 @@
+n, s = map(int, input().split())
+li = list(map(int, input().split()))
+
+ans = 1e9
+for i in range(n):
+    total = 0
+    for j in range(i, n):
+        total += li[j]
+        if total >= s:
+            ans = min(ans, j-i+1)
+
+if ans < 1e9:
+    print(ans)
+else:
+    print(-1)
