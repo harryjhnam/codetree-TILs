d1398209d738d31be1e07a1cc3de4b9a8add8cef
JuhyeonNam <harryjhnam@gist.ac.kr>
2024-07-08 09:05:36 +0900

    Title: 차이가 가장 작은 수, Time: 1330ms, Memory: 38MB, Status: Passed - CodeTree

--- 240708/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
+++ 240708/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
@@ -1,26 +1,19 @@
 from sortedcontainers import SortedSet
 
 n, m = map(int, input().split())
+s = SortedSet([int(input()) for _ in range(n)])
 
-s = SortedSet([int(input())])
 res = 1e9
-for _ in range(n-1):
-    x = int(input())
-    l_idx = s.bisect_left(x)
-    r_idx = s.bisect_right(x)
-    while l_idx >=0 or r_idx < len(s):
-        if 0 <= l_idx < len(s) and abs(x - s[l_idx]) >= m:
-            res = min(res, abs(x - s[l_idx]))
-        if r_idx < len(s) and abs(x - s[r_idx]) >= m:
-            res = min(res, abs(x - s[r_idx]))
-        
-        l_idx -= 1
-        r_idx += 1
-        
-        s.add(x)
-
-        if res == m:
-            break   
+for x in s:
+    min_idx = s.bisect_left(x+m)
+    if min_idx < len(s):
+        res = min(res, s[min_idx] - x)
+    max_idx = s.bisect_left(x-m) - 1
+    if max_idx >= 0:
+        res = min(res, x - s[max_idx])
+    
+    if res == m:
+        break
 
 if res == 1e9:
     print(-1)
