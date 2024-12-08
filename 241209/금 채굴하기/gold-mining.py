b0b66fc54c2dc40c5294832b6270d5945788f29d
JuhyeonNam <harryjhnam@gist.ac.kr>
2024-12-09 08:40:16 +0900

    Title: 금 채굴하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241209/금 채굴하기/gold-mining.py
@@ -0,0 +1,39 @@
+n, gold_cost = map(int, input().split())
+mat = []
+for _ in range(n):
+    mat.append(list(map(int, input().split())))
+
+def get_mining_area(i, j, k):
+    res = set()
+    for i_range in range(k+1):
+        for j_range in range(k-i_range+1):
+            res.add( (i-i_range, j-j_range) )
+            res.add( (i-i_range, j+j_range) )
+            res.add( (i+i_range, j-j_range) )
+            res.add( (i+i_range, j+j_range) )
+    return list(res)
+
+def is_valid_corr(i, j, n):
+    return 0<=i<n and 0<=j<n
+
+def is_profitable(n_golds, gold_cost, n_area):
+    return n_golds*gold_cost >= n_area
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(n//2, 0, -1):
+            mining_area = get_mining_area(i, j, k)
+            n_area = len(mining_area)
+            n_golds = 0
+            for x, y in mining_area:
+                if not is_valid_corr(x, y, n):
+                    continue
+                n_golds += mat[x][y]
+            if n_golds < ans:
+                break
+            if is_profitable(n_golds, gold_cost, n_area):
+                ans = n_golds
+
+print(ans)
+
