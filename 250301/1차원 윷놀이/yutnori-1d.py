76bdbe8fadf448dff93e8b1bd4782640296a7fcb
JuhyeonNam <harryjhnam@gist.ac.kr>
2025-03-01 22:33:47 +0900

    Title: 1차원 윷놀이, Time: 56ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250301/1차원 윷놀이/yutnori-1d.py
+++ 250301/1차원 윷놀이/yutnori-1d.py
@@ -4,16 +4,24 @@ nums = list(map(int, input().split()))
 # Please write your code here.
 locs = [0] * k
 answer = 0
+
+def calc():
+    global locs
+    score = 0
+    for loc in locs:
+        score += (loc >= m-1)
+    return score
+
 def make_move(n_moves):
     global locs, nums, answer
     
-    answer = max(answer, len([loc for loc in locs if loc >= m-1]))
+    answer = max(answer, calc())
 
     if n_moves == n:
         return
 
     for i in range(k):
-        if locs[i] >= m:
+        if locs[i] >= m-1:
             continue
         locs[i] += nums[n_moves]
         make_move(n_moves + 1)
