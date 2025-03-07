752a37183c0c63f3dee6ae2f02d4a1508e811531
JuhyeonNam <harryjhnam@gist.ac.kr>
2025-03-07 16:31:55 +0900

    Title: 피보나치 수, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250307/피보나치 수/fibonacci-number.py
+++ 250307/피보나치 수/fibonacci-number.py
@@ -5,8 +5,6 @@ N = int(input())
 # f(1) = 1, f(2) = 1
 
 memo = [-1] * (N+1)
-memo[1] = 1
-memo[2] = 1
 
 def fibo(n):
     global memo
@@ -14,7 +12,10 @@ def fibo(n):
     if memo[n] != -1:
         return memo[n]
 
-    memo[n] = fibo(n-1) + fibo(n-2)
+    if n <= 2:
+        memo[n] = 1
+    else:
+        memo[n] = fibo(n-1) + fibo(n-2)
 
     return memo[n]
 
